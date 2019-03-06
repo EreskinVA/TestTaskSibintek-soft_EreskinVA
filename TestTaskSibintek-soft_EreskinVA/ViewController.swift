23394bc1f50c4f4b2456f7d064df9de79b099c18
EreskinVA <ereskinva@icloud.com>
2019-03-06 10:46:42 +0300

    Initial Commit

---
+++ TestTaskSibintek-soft_EreskinVA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTaskSibintek-soft_EreskinVA
+//
+//  Created by Vladimir Ereskin on 06/03/2019.
+//  Copyright © 2019 Vladimir Ereskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
